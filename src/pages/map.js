7094337be746936775cc0add09a8bd527bc6478a
blaineanthonywestern <blaine.a.western@gmail.com>
2023-01-20 22:02:03 -0500

    Chore: run npm install

---
+++ src/pages/map.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// Refer to the following documentation to get started here: https://visgl.github.io/react-map-gl/docs/get-started/get-started
+
+export default function Map() {
+  return (
+    <div>map</div>
+  )
+}
