3b218ca7a8ecb639de6dceeaa5a8ea61770b3839
blaineanthonywestern <blaine.a.western@gmail.com>
2023-02-11 10:25:56 -0500

    Chore: resolve strapi version

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,5 +1,47 @@
-// Refer to the following documentation to get started here: https://visgl.github.io/react-map-gl/docs/get-started/get-started
+import Map from 'react-map-gl'
+
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            geometry: { type: 'Point', coordinates: [174.075226, -39.055626] },
+            properties: {
+                description: 'Sample Person'
+            }
+        }
+    ]
+}
+
+const layerStyle = {
+    id: 'places',
+    type: 'circle',
+    paint: {
+        'circle-radius': 10,
+        'circle-color': '#007cbf'
+    }
+}
+
+// for handlers see the following: https://visgl.github.io/react-map-gl/docs/api-reference/map#onclick
 
 export default function Map() {
-    return <div>map</div>
+    return (
+        <div>
+            <Map
+                initialViewState={{
+                    longitude: 174.075226,
+                    latitude: -39.055626,
+                    zoom: 14
+                }}
+                style={{ width: 600, height: 400 }}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                onClick={(e) => console.log({ e })}
+                interactiveLayerIds={["'places"]}
+            >
+                <Source id="my-data" type="geojson" data={geojson}>
+                    <Layer {...layerStyle} />
+                </Source>
+            </Map>
+        </div>
+    )
 }
