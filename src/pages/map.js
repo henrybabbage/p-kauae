982f9b3ca04a570b5f15c60efbc94ddd3340c7aa
blaineanthonywestern <blaine.a.western@gmail.com>
2023-02-13 12:41:24 -0500

    Chore: Add map to app

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,47 +1,11 @@
-import Map, { Source, Layer } from 'react-map-gl'
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import Map from '@/components/map'
 
-const geojson = {
-    type: 'FeatureCollection',
-    features: [
-        {
-            type: 'Feature',
-            geometry: { type: 'Point', coordinates: [174.075226, -39.055626] },
-            properties: {
-                description: 'Sample Person'
-            }
-        }
-    ]
-}
-
-const layerStyle = {
-    id: 'places',
-    type: 'circle',
-    paint: {
-        'circle-radius': 10,
-        'circle-color': '#007cbf'
-    }
-}
-
-// for handlers see the following: https://visgl.github.io/react-map-gl/docs/api-reference/map#onclick
-
-export default function FullMap() {
+export default function map() {
     return (
-        <div>
-            <Map
-                initialViewState={{
-                    longitude: 174.075226,
-                    latitude: -39.055626,
-                    zoom: 14
-                }}
-                style={{ width: 600, height: 400 }}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-                onClick={(e) => console.log({ e })}
-                interactiveLayerIds={["'places"]}
-            >
-                <Source id="my-data" type="geojson" data={geojson}>
-                    <Layer {...layerStyle} />
-                </Source>
-            </Map>
-        </div>
+        <Box w="100%" h="100%">
+            <Map />
+        </Box>
     )
 }
