d733b6bf78468aa8313a07ad2e558b3cf04fc8b7
Henry Babbage <hbabbage@gmail.com>
2023-02-20 09:36:08 +0100

    Chore: add layout to map page

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,5 +1,16 @@
 // Refer to the following documentation to get started here: https://visgl.github.io/react-map-gl/docs/get-started/get-started
 
+import Header from '@/components/Header'
+import Layout from '@/components/Layout'
+import { Box } from '@chakra-ui/react'
+
 export default function Map() {
-    return <div>map</div>
+    return (
+        <main>
+            <Layout>
+                <Header />
+                <Box p="6" id="map"></Box>
+            </Layout>
+        </main>
+    )
 }
