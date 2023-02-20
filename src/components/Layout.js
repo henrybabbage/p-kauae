a90eabed481ab3b080d61fd74c5fe6fb353ffcee
Henry Babbage <hbabbage@gmail.com>
2023-02-20 10:43:12 +0100

    move meta tag info to layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,42 @@
 import { Box } from '@chakra-ui/react'
+import Head from 'next/head'
+
+export const siteTitle = 'Tū Tama Wāhine o Taranaki'
 
 const Layout = ({ children }) => {
     return (
-        <Box
-            bg="white"
-            w="100vw"
-            h="100vh"
-            display="flex"
-            flexDirection="column"
-        >
-            {children}
-        </Box>
+        <>
+            <Head>
+                <title>{siteTitle}</title>
+                <meta
+                    name="description"
+                    content="Website for the Pūkauae Photographic exhibition"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="data:," />
+                <meta
+                    name="description"
+                    content="Tū Tama Wāhine o Taranaki is a tangata whenua development and liberation organisation."
+                />
+                <meta
+                    name="author"
+                    content="Henry Babbage, Luke Enoka, Rere-No-A-Rangi, Blaine Western"
+                />
+                <meta name="keywords" content="art" />
+            </Head>
+            <Box
+                bg="white"
+                w="100vw"
+                h="100vh"
+                display="flex"
+                flexDirection="column"
+            >
+                {children}
+            </Box>
+        </>
     )
 }
 
