fe00cb54de5b8a53ee611a8e4fc5a4b80faa79b2
popere <rere.pope@vuw.ac.nz>
2023-02-21 17:53:51 +1300

    remove non-necessary content, adding wāhine and their connections to wāhi

--- backend/src/api/kamera/controllers/kamera.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * kamera controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::kamera.kamera');
