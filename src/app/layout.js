33644b9bf77e23f4d2d7c3fd0d04b838aece74de
Henry Babbage <hbabbage@gmail.com>
2023-03-13 13:35:31 +0100

    fix white background flash by setting background colour in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,12 +7,13 @@ import Fonts from '@/styles/Fonts'
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 import { AnimatePresence } from 'framer-motion'
+import styles from '@/styles/globals.css'
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head />
-            <body>
+            <body className={styles.background}>
                 <CacheProvider>
                     <ChakraProvider theme={theme}>
                         <Fonts />
