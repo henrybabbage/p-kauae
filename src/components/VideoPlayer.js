0927dd17faff5bb0c537a71e6fae12995eef147b
Henry Babbage <hbabbage@gmail.com>
2023-03-25 19:23:17 +0100

    add location over video

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,8 +1,9 @@
-import { AspectRatio } from '@chakra-ui/react'
+import { AspectRatio, Box, Heading } from '@chakra-ui/react'
 import '@vime/core/themes/default.css'
 import { ClickToPlay, Player, Poster, Spinner, Ui, Video } from '@vime/react'
 
 export default function VideoPlayer({
+    location,
     src,
     alt,
     poster,
@@ -13,17 +14,30 @@ export default function VideoPlayer({
 }) {
     const videoSrc = `${baseUrlVideo}${src}`
     return (
-        <AspectRatio maxH="75vh" ratio={16 / 9} cursor="crosshair">
-            <Player playsinline aspectRatio muted loop autoplay>
-                <Video>
-                    <source src={videoSrc} type="video/mp4" />
-                </Video>
-                <Ui>
-                    <ClickToPlay />
-                    <Spinner />
-                    <Poster />
-                </Ui>
-            </Player>
-        </AspectRatio>
+        <Box position="relative">
+            <AspectRatio maxH="75vh" ratio={16 / 9} cursor="crosshair">
+                <Player playsinline aspectRatio muted loop autoplay>
+                    <Video>
+                        <source src={videoSrc} type="video/mp4" />
+                    </Video>
+                    <Ui>
+                        <ClickToPlay />
+                        <Spinner />
+                        <Poster />
+                    </Ui>
+                </Player>
+            </AspectRatio>
+            <Heading
+                fontSize="36px"
+                color="white"
+                fontWeight="regular"
+                fontFamily="heading"
+                position="absolute"
+                bottom={6}
+                right={6}
+            >
+                {location}
+            </Heading>
+        </Box>
     )
 }
