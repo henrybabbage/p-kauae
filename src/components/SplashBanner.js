3f95a779847dce118865533e62e21b51f623dc47
Henry Babbage <hbabbage@gmail.com>
2023-03-26 20:17:41 +0200

    rename component

--- src/components/SplashBanner.js
+++ src/components/SplashBanner.js
@@ -1,7 +1,7 @@
 import { Flex, Heading, VStack } from '@chakra-ui/react'
 import Balancer from 'react-wrap-balancer'
 
-export default function WelcomeBanner() {
+export default function SplashBanner() {
     return (
         <VStack
             w="100vw"
