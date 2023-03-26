a03e82f755e4954c24f88ea7f5670b313fc4a8f4
Henry Babbage <hbabbage@gmail.com>
2023-03-26 15:28:52 +0200

    wip working out cors errors and testing 1080p

--- src/components/LandingVideo.js
+++ src/components/LandingVideo.js
@@ -1,3 +1,102 @@
-export default function LandingVideo() {
-    return <div>LandingVideo</div>
+import { AspectRatio, Box, Grid, Heading, Text } from '@chakra-ui/react'
+import '@vime/core/themes/default.css'
+import {
+    Controls,
+    LoadingScreen,
+    PlaybackControl,
+    Player,
+    Poster,
+    Ui,
+    Video
+} from '@vime/react'
+import Image from 'next/image'
+
+export default function LandingVideo({
+    src,
+    alt,
+    poster,
+    baseUrlVideo,
+    autoPlay,
+    muted,
+    loop
+}) {
+    const videoSrc = `${baseUrlVideo}${src}`
+    return (
+        <Box position="relative">
+            <AspectRatio
+                maxH="75vh"
+                maxW="100vw"
+                ratio={16 / 9}
+                cursor="crosshair"
+                display="flex"
+                justifyContent="center"
+            >
+                <Player
+                    playsinline
+                    aspectRatio
+                    preload
+                    muted={muted}
+                    loop={loop}
+                    autoplay={autoPlay}
+                >
+                    <Video>
+                        <source src={videoSrc} type="video/mp4" />
+                    </Video>
+                    <Ui>
+                        <Controls pin="center">
+                            <PlaybackControl
+                                hideTooltip
+                                style={{ '--vm-control-scale': 1.7 }}
+                            />
+                        </Controls>
+                        <LoadingScreen hideDots>
+                            <Grid
+                                bg="transparent"
+                                maxW="100vw"
+                                h="100vh"
+                                justifyContent="center"
+                                alignContent="center"
+                                gridAutoColumns="row"
+                            >
+                                <Image
+                                    src="/icons/pukauae.svg"
+                                    alt="Pukauae logo"
+                                    width="150"
+                                    height="150"
+                                    priority
+                                    sizes="100vw"
+                                    style={{
+                                        objectFit: 'contain',
+                                        objectPosition: 'center'
+                                    }}
+                                />
+                                <Text
+                                    pt="6"
+                                    fontFamily="body"
+                                    fontSize="16px"
+                                    color="white"
+                                    textTransform="uppercase"
+                                    textAlign="center"
+                                >
+                                    Loading
+                                </Text>
+                            </Grid>
+                        </LoadingScreen>
+                        <Poster />
+                    </Ui>
+                </Player>
+            </AspectRatio>
+            <Heading
+                fontSize="36px"
+                color="white"
+                fontWeight="regular"
+                fontFamily="heading"
+                position="absolute"
+                bottom={6}
+                right={6}
+            >
+                {location}
+            </Heading>
+        </Box>
+    )
 }
