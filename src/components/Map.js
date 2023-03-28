5ea978378af4d8a6d4f8ba2b5829e6e8756cb8f5
Henry Babbage <hbabbage@gmail.com>
2023-03-28 22:16:56 +0200

    remove drag from map

--- src/components/Map.js
+++ src/components/Map.js
@@ -19,8 +19,8 @@ export default function Map({ data }) {
         scrollZoom: false,
         boxZoom: false,
         doubleClickZoom: false,
-        dragRotate: true,
-        dragPan: true
+        dragRotate: false,
+        dragPan: false
     })
     const [mapData, setMapData] = useState(null)
 
