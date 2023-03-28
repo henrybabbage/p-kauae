48c510372e63d3503ece82defc541fcfa201fff5
Henry Babbage <hbabbage@gmail.com>
2023-03-28 19:10:13 +0200

    add transition to landing video overlay

--- src/components/LandingVideo.js
+++ src/components/LandingVideo.js
@@ -73,7 +73,7 @@ export default function LandingVideo({
                     justifyContent="center"
                     alignItems="center"
                 >
-                    {showOverlay && <VideoOverlay />}
+                    <VideoOverlay show={showOverlay} />
                     <Tooltip
                         label={
                             !isPlaying
