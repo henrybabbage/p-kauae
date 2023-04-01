621cb60907eef5b191db3eed32a24186ddc63916
Henry Babbage <hbabbage@gmail.com>
2023-04-01 19:21:22 +0200

    add video cover image for use later

---
+++ src/components/VideoCover.js
@@ -0,0 +1,29 @@
+import { Box, Grid } from '@chakra-ui/react'
+import Image from 'next/image'
+
+export default function VideoCover() {
+    return (
+        <Box position="absolute">
+            <Grid
+                z="10"
+                w="100%"
+                h="75vh"
+                placeSelf="center"
+                position="relative"
+                overflow="hidden"
+            >
+                <Image
+                    src="/images/video.jpg"
+                    alt="video preview image"
+                    width="1200"
+                    height="592"
+                    sizes="100vw"
+                    style={{
+                        objectFit: 'contain',
+                        objectPosition: 'center'
+                    }}
+                />
+            </Grid>
+        </Box>
+    )
+}
