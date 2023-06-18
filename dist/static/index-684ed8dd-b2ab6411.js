3e04b627c6bcfb18496b79584c670dd338e02002
Henry Babbage <hbabbage@gmail.com>
2023-06-18 21:47:00 +0200

    sanity updates

--- dist/static/index-684ed8dd-b2ab6411.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-e98b3e3a-7503059e.js";import"./sanity-dec6dfe8.js";import"./json-inspector-eb3c61ea.js";export{m as DocumentPaneProvider,a as default};
