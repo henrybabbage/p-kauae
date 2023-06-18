d515da3b2fb61fbf17e4b13b883ee27242beb6ed
Henry Babbage <hbabbage@gmail.com>
2023-06-18 22:19:46 +0200

    build sanity queries

---
+++ dist/static/index-684ed8dd-fba67e09.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-e98b3e3a-47a37f0c.js";import"./sanity-a401568c.js";import"./json-inspector-1e8e5a28.js";export{m as DocumentPaneProvider,a as default};
