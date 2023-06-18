0963c775132fac158cb9ec6ab562711f67f399b1
Henry Babbage <hbabbage@gmail.com>
2023-06-18 21:43:21 +0200

    add media browser plugin

---
+++ dist/static/index-684ed8dd-b2ab6411.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-e98b3e3a-7503059e.js";import"./sanity-dec6dfe8.js";import"./json-inspector-eb3c61ea.js";export{m as DocumentPaneProvider,a as default};
