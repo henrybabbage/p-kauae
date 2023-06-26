c7f5382dcf880ce118cb045291d0767a88d46fd8
Henry Babbage <hbabbage@gmail.com>
2023-06-26 11:20:14 +0200

    remove unused code

--- src/pages/haerenga.js
+++ src/pages/haerenga.js
@@ -16,65 +16,6 @@ export default function Haerenga({ wahines, preview }) {
     )
 }
 
-// import fsPromises from 'fs/promises'
-// import path from 'path'
-// export async function getStaticProps() {
-//     const filePath = path.join(process.cwd(), '/public/backend_data_final.json')
-//     const jsonData = await fsPromises.readFile(filePath)
-//     const objectData = JSON.parse(jsonData)
-
-//     const wahines = objectData.wahine
-//     const haerengaKorero = objectData.tu_tama_korero.haerenga_korero
-//     const baseUrl = objectData.whakaahua_s3_bucket
-//     const baseUrlVideo = objectData.kiriata_cloudfront
-
-//     const wahinesImages = objectData.wahine.map(
-//         (wahinesUrls) => `${baseUrl}${wahinesUrls?.whakaahua?.original}`
-//     )
-
-//     const portraits = await Promise.all(
-//         wahinesImages.map(async (src) => {
-//             const { blurhash, img } = await getPlaiceholder(src)
-//             return {
-//                 ...img,
-//                 blurhash,
-//                 alternativeText: 'Wahine portrait photograph'
-//             }
-//         })
-//     ).then((values) => values)
-
-//     const posterUrls = wahines.map(
-//         (wahinesVideos) => `${baseUrl}${wahinesVideos?.kiriata?.poster}`
-//     )
-
-//     const posters = await Promise.all(
-//         posterUrls.map(async (src) => {
-//             const { blurhash, img } = await getPlaiceholder(src)
-//             return {
-//                 ...img,
-//                 blurhash,
-//                 alternativeText: 'Whenua photograph'
-//             }
-//         })
-//     ).then((values) => values)
-
-//     if (!wahines) {
-//         return {
-//             props: null,
-//             notFound: true
-//         }
-//     }
-//     return {
-//         props: {
-//             wahines,
-//             haerengaKorero,
-//             portraits,
-//             posters,
-//             baseUrlVideo: baseUrlVideo
-//         }
-//     }
-// }
-
 export async function getStaticProps({ preview = false }) {
     if (preview) {
         return { props: { preview } }
