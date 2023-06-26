a86992ff356e26512149d147409f16d6734905e9
Henry Babbage <hbabbage@gmail.com>
2023-06-26 18:38:25 +0200

    add tokens

--- sanity/lib/sanity.client.js
+++ sanity/lib/sanity.client.js
@@ -10,29 +10,29 @@ export const client = createClient({
     studioUrl
 })
 
-export function getClient(preview) {
-    const client = createClient({
-        projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-        dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-        // apiVersion: '2022-11-11',
-        // @TODO perspectives require vX for now
-        apiVersion: 'X',
-        useCdn: true,
-        // token: process.env.SANITY_API_READ_TOKEN,
-        perspective: 'published',
-        studioUrl: '/studio',
-        logger: console
-    })
-    if (preview) {
-        if (!preview.token) {
-            throw new Error('You must provide a token to preview drafts')
-        }
-        return client.withConfig({
-            token: preview.token,
-            useCdn: false,
-            ignoreBrowserTokenWarning: true,
-            perspective: 'previewDrafts'
-        })
-    }
-    return client
-}
+// export function getClient(preview) {
+//     const client = createClient({
+//         projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//         dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+//         // apiVersion: '2022-11-11',
+//         // @TODO perspectives require vX for now
+//         apiVersion: 'X',
+//         useCdn: true,
+//         // token: process.env.SANITY_API_READ_TOKEN,
+//         perspective: 'published',
+//         studioUrl: '/studio',
+//         logger: console
+//     })
+//     if (preview) {
+//         if (!preview.token) {
+//             throw new Error('You must provide a token to preview drafts')
+//         }
+//         return client.withConfig({
+//             token: preview.token,
+//             useCdn: false,
+//             ignoreBrowserTokenWarning: true,
+//             perspective: 'previewDrafts'
+//         })
+//     }
+//     return client
+// }
