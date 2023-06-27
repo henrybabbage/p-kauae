01d7b15fbe8c6676cd12a0cc256673176fb0e0e9
Henry Babbage <hbabbage@gmail.com>
2023-06-27 09:25:52 +0200

    add missing tuhinga_matua field to query

---
+++ dist/static/index-aa34881c-812b20ac.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-37eb7c2e-5856a91a.js";import"./sanity-3c473299.js";import"./json-inspector-a8bb93a6.js";export{m as DocumentPaneProvider,a as default};
