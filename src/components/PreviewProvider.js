ee2c3ccf65f9f0af85848c25d295ff44b7e3a48b
Henry Babbage <hbabbage@gmail.com>
2023-07-24 16:13:01 +0200

    preview mode updates

--- src/components/PreviewProvider.js
+++ src/components/PreviewProvider.js
@@ -2,7 +2,7 @@ import { getClient } from '../../sanity/lib/sanity.client'
 import { LiveQueryProvider } from 'next-sanity/preview'
 import { useMemo } from 'react'
 
-export default function PreviewProvider({ children, token }) {
-    const client = useMemo(() => getClient({ token }), [token])
+export default function PreviewProvider({ children, previewToken }) {
+    const client = useMemo(() => getClient(previewToken), [previewToken])
     return <LiveQueryProvider client={client}>{children}</LiveQueryProvider>
 }
