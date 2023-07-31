bf65c96e56385c4cc90b46969d56b04d97894632
Henry Babbage <hbabbage@gmail.com>
2023-07-31 09:58:35 +0200

    try korero instead for preview

--- sanity/utils/defaultDocumentNode.js
+++ sanity/utils/defaultDocumentNode.js
@@ -3,7 +3,7 @@ import Iframe from 'sanity-plugin-iframe-pane'
 
 export const defaultDocumentNode = (S, { schemaType }) => {
     switch (schemaType) {
-        case `wahine`:
+        case `korero`:
             return S.document().views([
                 S.view.form(),
                 S.view
