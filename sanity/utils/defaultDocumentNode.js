6a7f41db77bbf9d4d1885c376ee13a647b776046
Henry Babbage <hbabbage@gmail.com>
2023-07-31 02:31:10 +0200

    add preview secret to env

--- sanity/utils/defaultDocumentNode.js
+++ sanity/utils/defaultDocumentNode.js
@@ -3,13 +3,16 @@ import Iframe from 'sanity-plugin-iframe-pane'
 
 export const defaultDocumentNode = (S, { schemaType }) => {
     switch (schemaType) {
-        case `movie`:
+        case `wahine`:
             return S.document().views([
                 S.view.form(),
                 S.view
                     .component(Iframe)
                     .options({
-                        url: `http://localhost:3000/api/preview`
+                        URL: (doc) =>
+                            doc?.slug?.current
+                                ? `http://localhost:3000/api/preview?slug=${doc.slug.current}`
+                                : `http://localhost:3000/api/preview`
                     })
                     .title('Preview')
             ])
