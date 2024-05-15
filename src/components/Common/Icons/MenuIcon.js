f2209d0f2b6534488dd3f770d220dc985ca590b9
Henry Babbage <hbabbage@gmail.com>
2024-05-16 01:09:00 +0200

    normalize font sizes

--- src/components/Common/Icons/MenuIcon.js
+++ src/components/Common/Icons/MenuIcon.js
@@ -6,7 +6,7 @@ export default function MenuIcon({ openDrawer }) {
             <Flex direction="column" alignItems="center">
                 <Heading
                     as="h2"
-                    fontSize={['18px', '18px', '18px', '20px', '20px', '20px']}
+                    fontSize={['24px', '24px', '24px', '32px', '32px', '32px']}
                     lineHeight="1.36"
                     fontFamily={['body', 'body', 'body', 'subheading', 'subheading', 'subheading']}
                     fontWeight="normal"
